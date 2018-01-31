dda75df026459c8549a984374779bdfa37de216e
Harris Dizdarevic <hdizdarevic9@gmail.com>
2018-01-31 13:26:00 -0500

    Completed HW1-Question1; initial commit

---
+++ HW1/QuestionOne.java
@@ -0,0 +1,16 @@
+/*
+ * Harris Dizdarevic
+ * CISC. 3150
+ * Question 1: Write a statement describing that I read the syllabus
+ *             and adhere to the academic integrity guidelines
+ */
+
+public class QuestionOne{
+
+    public static void main(String[] args){
+        System.out.println("I, Harris Dizdarevic, adhere to the academic integrity guidelines.");
+    }
+}
+
+
+
