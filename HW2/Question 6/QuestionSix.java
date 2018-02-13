42a0fb3f3a477e7182ed77ae09d6ca6117c5537b
Harris Dizdarevic <hdizdarevic9@gmail.com>
2018-02-13 11:14:19 -0500

    Question 6 HW2: Completed

---
+++ HW2/Question 6/QuestionSix.java
@@ -0,0 +1,19 @@
+/*
+ * Harris Dizdarevic
+ * CISC. 3150
+ * Question 6: In our class, we talked about how the Scanner class doesn't have a method
+ * for tokenizing individual characters. But you're hell bent on forcing the
+ * Scanner class to tokenize the individual characters. What should you set
+ * your delimiter to?
+ */
+
+import java.util.Scanner;
+
+public class QuestionSix{
+    public static void main(String[] args){
+        Scanner userInput = new Scanner(System.in);
+        userInput.useDelimiter("");
+        while(userInput.hasNext())
+            System.out.println(userInput.next());
+    }
+}
