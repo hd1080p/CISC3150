1125f6755e745d2fe42c8df48fde2aabd4e79f4a
Harris Dizdarevic <hdizdarevic9@gmail.com>
2018-02-21 13:46:56 -0500

    toUpperCase(), toLowerCase(), toString(), compareTo() Completed, valueOf() is left

--- HW3/Driver.java
+++ HW3/Driver.java
@@ -6,15 +6,46 @@
 
 import mystring.MyString;
 
+
 public class Driver{
     public static void main(String[] args){
         char[] testChar = {
-            't','e','s','t','i','n','g'
+            'T','e','S','t','I','n','G'
+        };
+
+        char[] testCharTwo = {
+            'a','b','c','d','e','f','g'
         };
 
         MyString test = new MyString(testChar);
+        MyString testTwo = new MyString(testCharTwo);
+        MyString testThree = new MyString(testChar);
+
         System.out.println("Length of MyString is: " + test.length());
         System.out.println("CharAt test: " + test.charAt(4));
-        System.out.println("Printing string: " + test.toString());
+
+        System.out.println("Printing String");
+        for(int i = 0; i < test.length(); i++)
+            System.out.printf("%c", test.charAt(i));
+
+        System.out.printf("\n");
+
+        System.out.println("Printing Substring");
+        for(int i = 0; i < test.substring(0,3).length(); i++)
+            System.out.printf("%c", test.substring(0,3).charAt(i));
+        System.out.printf("\n");
+
+        System.out.println("Testing toString() method");
+        System.out.println(test.toString());
+
+        System.out.println("Testing toLowerCase() method");
+        System.out.println(test.toLowerCase());
+
+        System.out.println("Testing toUpperCase() method");
+        System.out.println(testTwo.toUpperCase());
+
+        System.out.println("Testing compareTo");
+        System.out.println(test.compareTo(testTwo));
+        System.out.println(test.compareTo(testThree));
     }
 }
--- HW3/mystring/MyString.java
+++ HW3/mystring/MyString.java
@@ -11,7 +11,7 @@
  *   public MyString substring(int begin, int end);
  *   public MyString toLowerCase();
  *   public MyString toUpperCase();
- *   public boolean equals(MyString s);
+ *   public int compareTo(MyString s);
  *   public MyString getMyString();
  *   public String toString();
  *   public static MyString valueOf(int i);
@@ -22,6 +22,19 @@
  * you may only use the String's constructor, but not the methods.
  *
  * Make sure to provide a driver class, too, and show the output.
+ *
+ * For toLowerCase() and toUpperCase() methods, since the strings we are
+ * assuming are ASCII and this would only apply to characters that are actual
+ * letters, since numbers have no upper/lower case. By looking at the ASCII
+ * ranges of the characters I can tell whether to convert the character into a
+ * lower or upper case. This is done so by casting the character to an integer
+ * to get the ASCII value and modifying the character by Adding/Subtracting 32
+ * (for lower/uppercase characters) then casting it back as a character to get
+ * the new character value.
+ *
+ * The link to the table of the ASCII characters that I use for reference can
+ * be found here: http://www.asciitable.com/
+ * 
  */
 
 package mystring;
@@ -38,36 +51,85 @@ public class MyString{
     public char charAt(int index){
         if(index < 0 || index > this.length)
             throw new ArrayIndexOutOfBoundsException();
-        return this.arr[index];
+        MyString copy = this;
+        return copy.arr[index];
     }
 
     public int length(){
-        return this.arr.length;
+        MyString copy = this;
+        return copy.arr.length;
     }
 
-/*    public MyString substring(int begin, int end){
+    public MyString substring(int begin, int end){
+        MyString copy = this;
+        char[] newString = new char[(end-begin)];
+        int j = 0;
+        for(int i = begin; i < end; i++){
+            newString[j] = copy.charAt(i);
+            j++;
+        }
 
+        return new MyString(newString);
     }
 
     public MyString toLowerCase(){
-
+        MyString copy = this;
+        char[] lowerCase = new char[copy.length()];
+        for(int i = 0; i < copy.length(); i++){
+            //Redundant way to create a lowercased string
+            lowerCase[i] = copy.charAt(i);
+            if((int)copy.charAt(i) > 64 && (int)copy.charAt(i) < 91)
+                lowerCase[i] = (char)((int)copy.charAt(i) + 32);
+        }
+        copy = new MyString(lowerCase);
+        return copy;
     }
 
     public MyString toUpperCase(){
-
-    }
-
-    public boolean equals(MyString string){
-
+        MyString copy = this;
+        char[] upperCase = new char[copy.length()];
+        for(int i = 0; i < copy.length(); i++){
+            upperCase[i] = copy.charAt(i);
+            if((int)copy.charAt(i) > 96 && (int)copy.charAt(i) < 123)
+                upperCase[i] = (char)((int)copy.charAt(i) - 32);
+        }
+        copy = new MyString(upperCase);
+        return copy;
     }
 
     public String toString(){
-
+        MyString copy = this;
+        char[] newString = new char[copy.length()];
+        int j = 0;
+        for(int i = 0; i < copy.length(); i++){
+            newString[j] = copy.charAt(i);
+            j++;
+        }
+        return new String(newString);
     }
 
+    private int totalMyStringValue(MyString string){
+        //Sums up the total ASCII value of the string to be used as an
+        //auxillary for compareTo()
+        int sum = 0;
+        for(int i = 0; i < string.length(); i++)
+            sum += (int)string.charAt(i);
+        return sum;
+    }
+
+    public int compareTo(MyString string){
+        MyString copy = this;
+        if(this.totalMyStringValue(copy) < this.totalMyStringValue(string))
+            return -1;
+        else if(this.totalMyStringValue(copy) > this.totalMyStringValue(string))
+            return 1;
+        return 0;
+    }
+
+
+    /*        MyString copy = this;
     public static String valueOf(int i){
 
     }
 */
 }
-
